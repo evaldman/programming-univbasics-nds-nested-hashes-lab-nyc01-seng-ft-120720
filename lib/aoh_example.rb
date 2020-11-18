7cef26e2c1652ecb4ea34ee04437e2bd85fbe89d
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-18 02:21:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  dfw_array = []
+  dfw_array = [
+    { name:  "Don Gately", occupation: "Live-in Staff" }]
 end
 
 def aoh_lookup(aoh, row, key)
